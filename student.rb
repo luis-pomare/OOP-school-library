94150ac0e9f0d0272a1cdf5d478735b7fa01a69a
Luis Pomare <luispomare92@gmail.com>
2023-04-11 12:41:02 -0500

    Fix linters error

--- student.rb
+++ student.rb
@@ -5,7 +5,7 @@ require_relative './person'
 # Inherits from Person main class
 class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission:)
+    super(age, name, parent_permission)
     @classroom = classroom
   end
 
