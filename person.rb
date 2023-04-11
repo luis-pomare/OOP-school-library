f85699843307a753dc6b0a62f119706e0a390591
Luis Pomare <luispomare92@gmail.com>
2023-04-11 12:22:33 -0500

    Create student subclass

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@
 
 # Main class
 class Person
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -10,15 +10,17 @@ class Person
   end
 
   private
+
   def of_age?
     @age >= 18
   end
 
   public
+
   def can_use_services
     of_age? || @parent_permission
   end
-  
+
   attr_reader :id
   attr_accessor :name, :age
 end
