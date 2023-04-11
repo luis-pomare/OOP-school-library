cc00c88ced5f8777593dd8baaf1574e63ab44f29
Luis Pomare <luispomare92@gmail.com>
2023-04-11 09:51:05 -0500

    Reorder Person class arguments

--- person.rb
+++ person.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
+# Main class
 class Person
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  attr_reader :id
+  attr_accessor :name, :age
 end
