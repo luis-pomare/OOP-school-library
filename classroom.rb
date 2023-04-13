b89a7479fe40372c2837e8d833c9ecbc452ef79b
Luis Pomare <luispomare92@gmail.com>
2023-04-13 07:08:05 -0500

    Generate classroom-student association

--- classroom.rb
+++ classroom.rb
@@ -3,8 +3,15 @@
 # Classroom for students
 class Classroom
   attr_accessor :label
+  attr_reader :students
 
   def initialize(label)
     @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    @students.push(student) if @students.include?(student) == false
+    student.classroom = (self)
   end
 end
