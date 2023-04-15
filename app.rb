8eb0e0c6a339d9488f708c64a0c309f6a7bf662a
Luis Pomare <luispomare92@gmail.com>
2023-04-14 21:56:23 -0500

    Create available options for the user\'s menu

--- app.rb
+++ app.rb
@@ -10,28 +10,45 @@ require_relative './book'
 class App
   def initialize
     @running = 1
-    @imput = 1
+    @input = 0
   end
 
   def case1
-    puts 'All the objects you created will be deleted:'
+    puts "\nAll the objects you created will be deleted:"
     puts 'Are you sure you want to close the app? Y/N'
     @input = gets.chop
-    @running = 0 if @input.capitalize == 'Y'
+    if (@input.capitalize == 'Y' || @input.upcase == 'YES')
+      @running = 0
+    end
+    puts "\n"
+  end
+
+  def show_options
+    puts "Please select one of the following options:\n
+    1. For \"list all books\"\n
+    2. For \"list all students and teachers\"\n
+    3. For \"create a new library user\"\n
+    4. For \"insert a new book\"\n
+    5. For \"new rental\"\n
+    6. For \"list all rentals from a given persons' ID\"\n
+    7. For \"exit'\n
+    "
+  end
+
+  def default
+    puts "\nYou selected an invalid option\n"
   end
 
   def select
     case @input
-    when '1'
-      case1
-    else
-      puts 'Enter a valid option'
+    when '1' then case1
+    else default
     end
   end
 
   def run
     while @running == 1
-      puts 'Please select one option'
+      show_options
       @input = gets.chop
       select
     end
