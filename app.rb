452f09db009822342bceb53973156f98097d979e
Luis Pomare <luispomare92@gmail.com>
2023-04-14 23:06:52 -0500

    Organize app codebase

--- app.rb
+++ app.rb
@@ -17,35 +17,32 @@ class App
     puts "\nAll the objects you created will be deleted:"
     puts 'Are you sure you want to close the app? Y/N'
     @input = gets.chop
-    if (@input.capitalize == 'Y' || @input.upcase == 'YES')
-      @running = 0
-    end
+    @running = 0 if @input.capitalize == 'Y' || @input.upcase == 'YES'
     puts "\n"
   end
 
   def default
     puts "\nYou selected an invalid option\n"
   end
-  
+
   def select
     case @input
     when '7' then case7
     else default
     end
   end
-  
+
   def show_options
     puts "Please select one of the following options:\n
-    1. For \"list all books\"\n
-    2. For \"list all students and teachers\"\n
-    3. For \"create a new library user\"\n
-    4. For \"insert a new book\"\n
-    5. For \"new rental\"\n
-    6. For \"list all rentals from a given persons' ID\"\n
-    7. For \"exit'\n
-    "
+    1. For \"list all books\"
+    2. For \"list all students and teachers\"
+    3. For \"create a new library user\"
+    4. For \"insert a new book\"
+    5. For \"new rental\"
+    6. For \"list all rentals from a given persons' ID\"
+    7. For \"exit'\n"
   end
-  
+
   def run
     while @running == 1
       show_options
