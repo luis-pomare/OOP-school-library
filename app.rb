4a7e91e3c720be20d17d96f5573e5c40a3d8d651
Luis Pomare <luispomare92@gmail.com>
2023-04-17 15:29:44 -0500

    Refactor app to create a separate selection

--- app.rb
+++ app.rb
@@ -77,30 +77,4 @@ class App < Display
     person = read_rental_person
     @rentals.push(Rental.new(date, book, person))
   end
-
-  def select1
-    case @input
-    when '4' then insert_book
-    when '5' then insert_rental
-    when '6' then verify_rentals_ids
-    when '7' then exit_app
-    end
-  end
-
-  def select
-    case @input
-    when '1' then list_all_books
-    when '2' then list_users
-    when '3' then create_person
-    else select1
-    end
-  end
-
-  def run
-    while @running == 1
-      show_options
-      read_input
-      select
-    end
-  end
 end
