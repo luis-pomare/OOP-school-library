b1ee82b8bc89be83a8d3282a9243d4d345ffd57a
Kender ROMAIN <kenderromain@gmail.com>
2023-04-18 12:09:47 -0400

    Create preserve_rentals method.

--- database.rb
+++ database.rb
@@ -4,6 +4,7 @@ module Database
   def preserve_data
     preserve_books
     preserve_people
+    preserve_rentals
   end
 
   def preserve_books
@@ -16,6 +17,23 @@ module Database
     people_objects = []
     @students.each {|student| people_objects << {age: student.age, parent_permission: student.parent_permission, name: student.name, id: student.id, type: 'Student'}}
     @teachers.each {|teacher| people_objects << {age: teacher.age, specialization: teacher.specialization, name: teacher.name, id: teacher.id, type: 'Teacher'}}
-    File.write('people.json', people_objects)
+    File.write('people.json', people_objects.to_json)
   end
+
+  def preserve_rentals
+    rental_objects = []
+    @rentals.each do |rental|
+      # date, book, person
+      book = {title:rental.book.title,author: rental.book.author}
+      person = nil
+      if rental.person.class.name == "Student"
+        person = {age: rental.person.age, parent_permission: rental.person.parent_permission, name: rental.person.name, id: rental.person.id, type: 'Student'}
+      else
+        person = {age: rental.person.age, specialization: rental.person.specialization, name: rental.person.name, id: rental.person.id, type: 'Teacher'}
+      end
+      rental_objects << {date: rental.date,book: book,person:person}
+    end
+    File.write('rentals.json', rental_objects.to_json)
+  end
+
 end
